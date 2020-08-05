348189ef83ce2c4726bca4854e294b93bacea569
youngdon Jung <yeavov@gmail.com>
2020-08-05 17:57:05 +0900

    JPQL vs Querydsl

---
+++ src/main/java/study/querydsl/entity/Team.java
@@ -0,0 +1,29 @@
+package study.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username"})
+public class Team {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name){
+        this.name = name;
+    }
+}
