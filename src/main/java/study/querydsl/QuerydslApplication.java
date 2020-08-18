93e4db183e5b649204c0ba488a8b7a5bedd7e7fa
YoungdonJung <yeavov@gmail.com>
2020-08-18 18:37:00 +0900

    Interface

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -14,4 +14,9 @@ public class QuerydslApplication {
 		SpringApplication.run(QuerydslApplication.class, args);
 	}
 
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em){
+		return new JPAQueryFactory(em);
+	}
+
 }
